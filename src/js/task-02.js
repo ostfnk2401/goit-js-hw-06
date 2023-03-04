98d32e3f6f5315e912d527b4eed8a8e3d2ae7aa2
ostfnk2401 <o.stfnk2401@gmail.com>
2023-03-04 16:37:27 +0200

    second task done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ul = document.querySelector("#ingredients");
+
+const items = ingredients.map((ingredient) => {
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  li.classList.add("item");
+  return li;
+});
+
+ul.append(...items);
