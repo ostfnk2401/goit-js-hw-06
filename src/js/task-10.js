ad511c132257328c9d84ec56200504b229d41706
ostfnk2401 <o.stfnk2401@gmail.com>
2023-03-05 14:54:37 +0200

    tenth task done

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,28 @@
+const input = document.querySelector('#controls input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const amount = Number(input.value);
+  let boxes = '';
+
+  for (let i = 0, size = 30; i < amount; i += 1, size += 10) {
+    const color = getRandomHexColor();
+    const box = `<div style="background-color:${color};width:${size}px,height:${size}px"></div>`;
+    boxes += box;
+  }
+
+  boxesContainer.insertAdjacentHTML('beforeend', boxes);
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
