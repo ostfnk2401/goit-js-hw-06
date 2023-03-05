e8e56a4a053c252536c6c5aeafc6603ce53af592
ostfnk2401 <o.stfnk2401@gmail.com>
2023-03-05 14:33:14 +0200

    ninth task done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,15 @@
+const body = document.querySelector('body');
+const colorSpan = document.querySelector('.color');
+const changeColorButton = document.querySelector('.change-color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+changeColorButton.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorSpan.textContent = color;
+});
