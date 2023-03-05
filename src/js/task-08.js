cbc1919ba561c44de8f1645d016a88212de2dda0
ostfnk2401 <o.stfnk2401@gmail.com>
2023-03-05 14:26:10 +0200

    eighth task done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const email = loginForm.elements.email.value;
+    const password = loginForm.elements.password.value;
+
+    if (!email || !password) {
+        alert('All fields must be filled');
+        return
+    }
+
+    const loginData = {
+        email,
+        password
+    };
+
+    console.log(loginData);
+    loginForm.reset();
+});
