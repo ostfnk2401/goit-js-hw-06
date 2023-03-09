55d7cebc6870f82609f382cf05da8f676e4e14af
ostfnk2401 <o.stfnk2401@gmail.com>
2023-03-09 20:59:57 +0200

    first task done and fifth fixed

--- js/task-05.js
+++ js/task-05.js
@@ -1,10 +1,12 @@
 const nameInput = document.querySelector('#name-input');
 const nameOutput = document.querySelector('#name-output');
 
-nameInput.addEventListener('input', function() {
-  if (nameInput.value === '') {
+nameInput.addEventListener('input', onInputChange);
+
+function onInputChange() {
+  if (!nameInput.value) {
     nameOutput.textContent = 'Anonymous';
   } else {
     nameOutput.textContent = nameInput.value;
   }
-});
+}
